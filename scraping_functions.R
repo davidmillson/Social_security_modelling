5c8ef51ea3bfd584ec662d798b601d967a59f86f
David Millson <davidalmillson@gmail.com>
2016-10-09 16:19:21 +0100

    Change name to reflect increased scope

--- scraping_functions.R
+++ scraping_functions.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(lubridate)
 library(rvest)
+library(stringr)
 
 scrape_page = function(url) {
+  print(url)
   data = read_html(url)
   scraped = data %>% html_nodes("td.r.b.data") %>% html_text() %>% as.numeric()
   if(length(scraped) == 0) {
@@ -11,24 +13,44 @@ scrape_page = function(url) {
   return(scraped)
 }
 
-generate_url = function(quarter) {
-  url_template = "http://tabulation-tool.dwp.gov.uk/100pc/aa_ent/ccsex/ccaaawd/ccgor/a_carate_r_ccsex_c_ccaaawd_p_ccgor_scotland_%s.html"
-  return(sprintf(url_template, quarter))
+get_column_headers = function(url_minus_date) {
+  url_minus_date %>%
+    sprintf("May02") %>%
+    read_html() %>%
+    html_nodes(xpath = "//html//body//div[2]//table//thead//tr[2]//th") %>%
+    html_text(trim = TRUE) %>%
+    str_replace_all(" ","_")
 }
 
-scrape_quarter = function(quarter) {
-  return(quarter %>% generate_url %>% scrape_page)
+get_row_headers = function(url_minus_date) {
+  (url_minus_date %>%
+    sprintf("May02") %>%
+    read_html() %>%
+    html_nodes(xpath = "//html//body//div[2]//table//tbody//tr//th") %>%
+    html_text(trim = TRUE) %>%
+    str_replace_all(" ","_")
+  )[-2:-1]
 }
 
-dates_to_quarters = function(dates) {
-  return(dates %>% format("%b%y"))
+generate_url = function(quarter, url_minus_date) {
+  url_minus_date %>% sprintf(quarter) %>% return()
 }
 
-get_time_series = function() {
+format_dates = function(dates) {
+  return(dates %>% format("%b%y") %>% purrr::set_names())
+}
+
+get_time_series = function(benefit, analysis, row, column, subset_on, subset = NULL) {
+  url_template = "http://tabulation-tool.dwp.gov.uk/100pc/%1$s/%3$s/%4$s/%5$s/a_%2$s_r_%3$s_c_%4$s_p_%5$s_%6$s_%%s.html"
+  url_minus_date = url_template %>% sprintf(benefit, analysis, row, column, subset_on, subset)
+  row_headers = get_row_headers(url_minus_date)
+  column_headers = get_column_headers(url_minus_date)
   dates = ymd("2002-05-01") %m+% months(seq(from = 0, to = 165, by = 3))
   dates %>%
-    dates_to_quarters() %>%
-    sapply(scrape_quarter) %>%
+    format_dates() %>%
+    map_df(~generate_url(., url_minus_date) %>% scrape_page()) %>%
     as_tibble() %>%
+    #mutate_(row = c(rep("Female", 3), rep("Male", 3)), column = rep(column_headers, length(row_headers))) %>%
+    #gather(date, caseload, -gender, -rate) %>%
     return()
 }
